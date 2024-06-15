6da31c5e536c486731d65bc76d38cd80da8124dc
Riley King <kingr26@wwu.edu>
2024-06-15 01:11:53 -0700

    Implemented Image Model

--- GeekBook/GeekBook/urls.py
+++ GeekBook/GeekBook/urls.py
@@ -33,3 +33,5 @@ urlpatterns = [
     path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
