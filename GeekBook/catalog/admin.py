b88c5350d30cdd46354e104cef3768dc2b5369e4
Riley King <kingr26@wwu.edu>
2024-06-15 00:29:00 -0700

    Created Models forsite

--- GeekBook/catalog/admin.py
+++ GeekBook/catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Year, Description, Author
+
+admin.site.register(Year)
+admin.site.register(Description)
+admin.site.register(Author)
