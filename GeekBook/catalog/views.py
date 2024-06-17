9cab330aa80ac53c29d22c6e70d7c97c4b5587ff
Riley King <kingr26@wwu.edu>
2024-06-17 05:53:50 -0700

    Added Geek photos and names to home page with some formatting

--- GeekBook/catalog/views.py
+++ GeekBook/catalog/views.py
@@ -18,18 +18,32 @@ def index(request):
     # The 'all()' is implied by default.
     num_image= Image.objects.count()
 
+    team = [
+        {'name': 'Riley King', 'role': 'Member 1', 'image_url': '/static/images/member1.jpg'},
+        {'name': 'Rachel Strober', 'role': 'Member 2', 'image_url': '/static/images/member1.jpg'},
+        {'name': 'Piper Smith', 'role': 'Member 3', 'image_url': '/static/images/member1.jpg'},
+        {'name': 'Sara Morgan', 'role': 'Member 4', 'image_url': '/static/images/member1.jpg'},
+        {'name': 'Myrie Murphy', 'role': 'Member 5', 'image_url': '/static/images/member1.jpg'},
+        # Add more team members as needed...
+    ]
+
+    
     context = {
         'num_books': num_books,
         'num_instances': num_months,
         #'num_instances_available': num_instances_available,
         'num_authors': num_image,
+        'team':team
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
 
 class BookListView(generic.ListView):
     model = Book
 
 class BookDetailView(generic.DetailView):
     model = Book
+
